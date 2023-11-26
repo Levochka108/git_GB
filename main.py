3b27adae4dcd0f42e668772dc12098e36a910435
дмитрий клочков <d.klochkov9421@gmail.com>
2023-11-26 22:01:03 +0300

    update main.py, add new func division and update multilication

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-def func(x: int | float , y: int | float):
+def multiplication(x: int | float , y: int | float):
     result = x * y
     return result
 
+def division(x: int | float, y: int | float):
+    result = x / y
+    return result
+
 def printer():
-    print(f"{func(5, 5)} ")
-    print(f"{func(2.25, 5.5)}")
+    print(f"Произведино умножение {multiplication(5, 5)} ")
+    print(f"Проиведино умножение {multiplication(2.25, 5.5)}")
+    print(f"Произведино деление {division(25, 5)}")
+
 
 if __name__ == '__main__':
     printer()
