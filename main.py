669288906c603db99845835ec86d8a4473d2fd6f
DK9421108 <153086550+DK9421108@users.noreply.github.com>
2023-12-06 16:43:38 +0300

    Update main.py
    Dev-ubuntu task is done

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-def func(x: int | float , y: int | float):
+#Изменения в коде                               
+def multiplication(x: int | float , y: int | float):
     result = x * y
     return result
 
+def division(x: int | float, y: int | float):
+    result = x / y
+    return result
+def subtraction(x: int, y: int):
+    result = x - y
+    return result
+
 def printer():
-    print(f"{func(5, 5)} ")
-    print(f"{func(2.25, 5.5)}")
+    print(f"Произведино умножение {multiplication(5, 5)} ")
+    print(f"Проиведино умножение {multiplication(2.25, 5.5)}")
+    print(f"Произведино деление {division(25, 5)}")
+    print(f"Произведино вычитание {subtraction(10, 5)}")
 
 if __name__ == '__main__':
     printer()
